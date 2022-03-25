63ecd4343adaeb5484fc822bde4e69096c00f1de
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2022-03-25 18:56:21 +0100

    Update fila.h

--- fila.h
+++ fila.h
@@ -14,6 +14,7 @@ class Fila {
         void push(const Persona& dato);
         bool PersonPresent(const Persona& dato);
         Fila Split(const Persona& dato);
+        Persona pop(void);
 
     private:
         std::shared_ptr<Elemento> _ultimo = nullptr;
